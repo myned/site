c7eaa46987cd7c6d9e9b62acb9bb97fb68c1f7b4
Myned <dev@bjork.tech>
2026-03-22 13:24:28 -0500

    stream: fix stream url params
    Signed-off-by: Myned <dev@bjork.tech>

--- src/pages/stream/api/[id].json.ts
+++ src/pages/stream/api/[id].json.ts
@@ -4,18 +4,20 @@ export const prerender = false
 import base64url from "base64url"
 import { createHmac } from "crypto"
 
-const { PROD } = import.meta.env
 const { OME_HOST = "", OME_SECRET_KEY = "", PROXY_HOST = OME_HOST } = process.env
 
-export async function GET({ params }: any) {
-  const { app = "live", stream = "stream", playlist = "default" } = params
+export async function GET({ params, request }: any) {
+  const { id = "stream" } = params
+  const url = new URL(request.url)
+  const app = url.searchParams.get("app") ?? "live"
+  const playlist = url.searchParams.get("playlist") ?? "default"
 
   // https://docs.ovenmediaengine.com/access-control/signedpolicy
   // https://github.com/OvenMediaLabs/OvenMediaEngine/blob/master/misc/signed_policy_url_generator.js
   const expire = Date.now() + 24 * 60 * 60 * 1000 // Now + 24 hours
   const policy = base64url(Buffer.from(`{"url_expire": ${expire}}`)) // Common base64-encoded policy JSON
-  const webrtcUrl = `ws://${OME_HOST}/${app}/${stream}/${playlist}?policy=${policy}` // WebRTC policy URL
-  const llhlsUrl = `http://${OME_HOST}/${app}/${stream}/${playlist}.m3u8?policy=${policy}` // LLHLS policy URL
+  const webrtcUrl = `ws://${OME_HOST}/${app}/${id}/${playlist}?policy=${policy}` // WebRTC policy URL
+  const llhlsUrl = `http://${OME_HOST}/${app}/${id}/${playlist}.m3u8?policy=${policy}` // LLHLS policy URL
   const webrtcSignature = base64url(createHmac("sha1", OME_SECRET_KEY).update(webrtcUrl).digest()) // WebRTC base64-encoded HMAC
   const llhlsSignature = base64url(createHmac("sha1", OME_SECRET_KEY).update(llhlsUrl).digest()) // LLHLS base64-encoded HMAC
   const signedWebrtcUrl = `${webrtcUrl}&signature=${webrtcSignature}` // WebRTC signed URL
